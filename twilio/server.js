faa7993e3f466f24130101ddead09b609d1295a3
shahithbot <103890207+shahithbot@users.noreply.github.com>
2022-04-17 20:12:25 +0530

    Update server.js

--- twilio/server.js
+++ twilio/server.js
@@ -29,10 +29,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //See https://dialogflow.com/docs/reference/v2-auth-setup and
 // https://cloud.google.com/dialogflow/docs/setup for details.
 
-const projectId = 'Place your dialogflow projectId here';
-const phoneNumber = "Place your twilio phone number here";
-const accountSid = 'Place your accountSid here';
-const authToken = 'Place your authToken here';
+const projectId = 'examez-jsfg';
+const phoneNumber = "+12055741092";
+const accountSid = 'AC388b617f1a027f00c25e1209feada03d';
+const authToken = '1c8b13e605a47d4b2855cce6e4a4c7a2';
 
 const client = require('twilio')(accountSid, authToken);
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
